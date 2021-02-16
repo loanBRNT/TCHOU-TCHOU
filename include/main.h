df13ed80601f9127c960dffc690ad0fa47220a16
BERNAT Loan <loantravail@gmail.com>
2021-02-16 16:04:54 +0100

    first commit

---
+++ include/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/* 
+    Commande de compilation avec gcc :
+    gcc src/main.c -o bin/prog -I include -L lib -lmingw32 -lSDL2main -lSDL2
+    gcc src/* -o bin/prog -I include -L lib -lmingw32 -lSDL2main -lSDL2
+*/ 
+
+
+int main(int argc, char const *argv[])
+{
+	/* code */
+	return 0;
+}
