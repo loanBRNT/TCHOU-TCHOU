cd4b0ee987faa3f14ed7068bb38bca88cc6d0baf
BERNAT Loan <loantravail@gmail.com>
2021-02-23 14:59:58 +0100

    Start implementation reseau

--- src/main.c
+++ src/main.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
+#include "reseau.h"
 
 /* 
     Commande de compilation avec gcc :
     gcc src/main.c -o bin/prog -I include -L lib -lmingw32
-    gcc src/* -o bin/prog -I include -L lib -lmingw32
+    gcc src/* -o prog -I include -L lib -lmingw32
 */ 
 
 
 int main(int argc, char const *argv[])
 {
-	/* code */
+	initReseau();
 	return 0;
 }
