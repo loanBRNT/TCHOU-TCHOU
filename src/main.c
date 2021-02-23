06d4f72ea399b6873f1bd8b75029334b9b3d91cb
EliottLeS <eliott.gaudillat@univ-tlse3.fr>
2021-02-23 16:09:45 +0100

    avancer menu

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 #include "main.h"
 #include "pwd.h"
 #include "reseau.h"
+#include "menu.h"
 
 /* 
     Commande de compilation avec gcc :
@@ -13,8 +14,63 @@
 
 int main(int argc, char const *argv[])
 {
-
-	verifierPwdAdmin();
 	initReseau();
+	int event = 1 ;
+	int choixMenu;
+	printf("###########################################\n");
+	printf("#                                         #\n");
+	printf("#            1:ADMINISTRATEUR             #\n");
+	printf("#                                         #\n");
+	printf("#            2:CONTROLEUR                 #\n");
+	printf("#                                         #\n");
+	printf("#            3:CLIENT                     #\n");
+	printf("#                                         #\n");
+	printf("#            4:QUITTER                    #\n");
+	printf("#                                         #\n");
+	printf("###########################################\n");
+	printf("\n");
+	
+	while(event != -1){
+		printf("###########################################\n");
+		printf("#           choisissez un menu            #\n");
+		printf("###########################################\n");
+		printf("\n");
+		scanf("%d",&choixMenu);
+		printf("\n");
+		switch(choixMenu)
+		{
+			case 1:
+				printf("###########################################\n");
+				printf("# Vous avez choisi le menu ADMINISTRATEUR #\n");
+				printf("###########################################\n");
+				menuAdmin();
+				break;
+			case 2:
+				printf("###########################################\n");
+				printf("#   Vous avez choisi le menu CONTROLEUR   #\n");
+				printf("###########################################\n");
+				menuControleur();
+				break;
+			case 3:
+				printf("###########################################\n");
+				printf("#     Vous avez choisi le menu CLIENT     #\n");
+				printf("###########################################\n");
+				menuClient();
+				break;
+			case 4:
+				printf("###########################################\n");
+				printf("#       A BIENTOT SUR NOTRE RESEAU        #\n");
+				printf("###########################################\n");
+				event = -1;
+				break;
+			default:
+				printf("###########################################\n");
+				printf("# Vous n'avez pas choisi un menu correct  #\n");
+				printf("###########################################\n");
+				break;
+		}
+	printf("\n");
+	}
+
 	return 0;
 }
