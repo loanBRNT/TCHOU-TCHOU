acb9511048a58411aa7f0d3ef1e28e8a5561656b
EliottLeS <eliott.gaudillat@univ-tlse3.fr>
2021-04-03 22:13:41 +0200

    repush suite pb merge

--- src/menu.c
+++ src/menu.c
@@ -1143,7 +1143,6 @@ int menuModificationVoyage(Reseau r){
 	printf("\n");
 	scanf("%s", numClient);
 	fflush(stdin);
-	modifVoyageur(r, numClient);
 	int event = 1;
 	printf("\n");
 	printf("################################################\n");
